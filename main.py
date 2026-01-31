34610925ff5e67367361d68029ee167a75170fe2
wenjiewang <mail@tcox.org>
2026-01-31 13:07:01 +0000

    zap

--- main.py
+++ main.py
@@ -7,9 +7,22 @@ WINDOW_HEIGHT: int = 600
 
 PLAYER_WIDTH: int = 48
 PLAYER_HEIGHT: int = 48
+
+PROJECTILE_WIDTH: int = 48
+PROJECTILE_HEIGHT: int = 48
+
 GAP_BELOW_PLAYER: int = 24
 
 BORDERS = 200
+
+
+class Projectile:
+  def __init__(self, x, y, dy):
+    self.x = x
+    self.y = y
+    self.dy = dy
+
+projectiles = []
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
@@ -33,6 +46,11 @@ while True:  # main game loop
         player_x -= 1
     if keys[K_RIGHT]:
         player_x += 1
+    if keys[K_SPACE]:
+        # shoot mask
+        if not projectiles:
+            projectiles.append(Projectile(player_x, WINDOW_HEIGHT - PLAYER_HEIGHT - GAP_BELOW_PLAYER, -1))
+
 
     if player_x < 200:
         player_x = 200
@@ -42,6 +60,16 @@ while True:  # main game loop
 
     pygame.draw.rect(screen, (100, 100, 100),
                      pygame.Rect(0 + BORDERS, 0, WINDOW_WIDTH - BORDERS - BORDERS, WINDOW_HEIGHT))
+
+    remove_projectiles = []
+    for projectile in projectiles:
+        projectile.y += projectile.dy
+        pygame.draw.rect(screen, (255, 0, 0),
+                       pygame.Rect(projectile.x, projectile.y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT))
+        if projectile.y < -PROJECTILE_HEIGHT:
+            remove_projectiles.append(projectile)
+    for projectile in remove_projectiles:
+      projectiles.remove(projectile)
 
     pygame.draw.rect(
         screen,

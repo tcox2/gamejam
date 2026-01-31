4e5aeee8dd93e4c2af7b8fc41a62d5c541af33f2
wenjiewang <mail@tcox.org>
2026-01-31 12:21:37 +0000

    ADD BORDERS

--- main.py
+++ main.py
@@ -11,44 +11,42 @@ GAP_BELOW_PLAYER: int = 24
 
 BORDERS = 200
 
-
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 # space invader style character at bottom
 
 player_x = WINDOW_WIDTH / 2
 
-
 pygame.display.set_caption('Masketeer')
 
-while True: # main game loop
+while True:  # main game loop
 
-  # events
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
+    # events
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-  # key handling
-  keys = pygame.key.get_pressed()
-  if keys[K_LEFT]:
-    player_x -= 1
-  if keys[K_RIGHT]:
-    player_x += 1
+    # key handling
+    keys = pygame.key.get_pressed()
+    if keys[K_LEFT]:
+        player_x -= 1
+    if keys[K_RIGHT]:
+        player_x += 1
 
-  if player_x < 200:
-    player_x = 200
+    if player_x < 200:
+        player_x = 200
 
-  if player_x >= (max_x := (WINDOW_WIDTH - BORDERS - PLAYER_WIDTH)):
-    player_x = max_x
+    if player_x >= (max_x := (WINDOW_WIDTH - BORDERS - PLAYER_WIDTH)):
+        player_x = max_x
 
-  pygame.draw.rect(screen, (100, 100, 100), pygame.Rect(0 + BORDERS, 0 , WINDOW_WIDTH - BORDERS -BORDERS, WINDOW_HEIGHT))
+    pygame.draw.rect(screen, (100, 100, 100),
+                     pygame.Rect(0 + BORDERS, 0, WINDOW_WIDTH - BORDERS - BORDERS, WINDOW_HEIGHT))
 
-  pygame.draw.rect(
-    screen,
-    "blue",
-    (player_x, WINDOW_HEIGHT - PLAYER_HEIGHT - GAP_BELOW_PLAYER, PLAYER_WIDTH, PLAYER_HEIGHT)
-  )
+    pygame.draw.rect(
+        screen,
+        "blue",
+        (player_x, WINDOW_HEIGHT - PLAYER_HEIGHT - GAP_BELOW_PLAYER, PLAYER_WIDTH, PLAYER_HEIGHT)
+    )
 
-  pygame.display.update()
-
+    pygame.display.update()

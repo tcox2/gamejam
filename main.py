8cfbe35add55502f0bd18ef0dfeb79e5bb1ce2bf
wenjiewang <mail@tcox.org>
2026-01-31 15:04:46 +0000

    draw guest

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from ctypes.macholib.dylib import dylib_info
+
 import pygame, sys
 from pygame.locals import *
+import random
+
+DEBUG: bool = False
 
 pygame.init()
 WINDOW_WIDTH: int = 800
@@ -8,15 +13,18 @@ WINDOW_HEIGHT: int = 600
 PLAYER_WIDTH: int = 48
 PLAYER_HEIGHT: int = 48
 
+GUEST_WIDTH: int = 48
+GUEST_HEIGHT: int = 48
+
 PROJECTILE_WIDTH: int = 48
 PROJECTILE_HEIGHT: int = 48
 
 GAP_BELOW_PLAYER: int = 24
-TIME = 0
+time = 0
 AFTER_WHAT_TIME_NEW_GUEST_VISITS = 3000
-LAST_TIME_NEW_QUEST_VISITS = 0
 BORDERS = 200
 
+last_time_new_guest_visits = 0
 
 class Projectile:
   def __init__(self, x, y, dy):
@@ -25,6 +33,14 @@ class Projectile:
     self.dy = dy
 
 projectiles = []
+
+class Guest:
+  def __init__(self, x, y, dy):
+    self.x = x
+    self.y = y
+    self.dy = dy
+
+guests = []
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
@@ -72,24 +88,36 @@ while True:  # main game loop
     remove_projectiles = []
     for projectile in projectiles:
         projectile.y += projectile.dy
-        pygame.draw.rect(screen, (255, 0, 0),
+        pygame.draw.rect(screen, "yellow",
                        pygame.Rect(projectile.x, projectile.y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT))
         if projectile.y < -PROJECTILE_HEIGHT:
             remove_projectiles.append(projectile)
     for projectile in remove_projectiles:
       projectiles.remove(projectile)
 
-    TIME = pygame.time.get_ticks()
-    textSurfaceObj = fontObj.render(str(TIME), True, GREEN, BLUE)
-    textRectObj = textSurfaceObj.get_rect()
-    textRectObj.center = (200, 150)
-    screen.blit(textSurfaceObj, textRectObj)
+    time = pygame.time.get_ticks()
 
-    delta = TIME - LAST_TIME_NEW_QUEST_VISITS
-    print(TIME, LAST_TIME_NEW_QUEST_VISITS, delta)
-    if delta > AFTER_WHAT_TIME_NEW_GUEST_VISITS:
-      LAST_TIME_NEW_QUEST_VISITS = TIME
+    remove_guests = []
+    for guest in guests:
+      guest.y += guest.dy
+      if guest.y > WINDOW_HEIGHT + GUEST_HEIGHT:  # hit bottom of screen
+        remove_guests.append(guest)
+    for guest in remove_guests:
+      guests.remove(guest)
 
+    time_since_last_guest : int = time - last_time_new_guest_visits
+    if time_since_last_guest > AFTER_WHAT_TIME_NEW_GUEST_VISITS:
+      guests.append(Guest(random.randint(BORDERS, WINDOW_WIDTH-BORDERS-GUEST_WIDTH), 0, 1))
+      last_time_new_guest_visits = time
+
+    # draw
+
+    for guest in guests:
+      pygame.draw.rect(
+        screen,
+        "red",
+        (guest.x, guest.y, GUEST_WIDTH, GUEST_HEIGHT)
+      )
 
     pygame.draw.rect(
         screen,

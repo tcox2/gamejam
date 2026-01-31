8c313b977411bb6739474198478df96a4fa35ee1
wenjiewang <mail@tcox.org>
2026-01-31 15:36:52 +0000

    convert to rects

--- main.py
+++ main.py
@@ -6,52 +6,45 @@ import random
 
 DEBUG: bool = False
 
+WHITE = (255, 255, 255)
+LIGHT_GREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 128)
+
 pygame.init()
 WINDOW_WIDTH: int = 800
 WINDOW_HEIGHT: int = 600
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 PLAYER_WIDTH: int = 48
 PLAYER_HEIGHT: int = 48
-
-GUEST_WIDTH: int = 48
-GUEST_HEIGHT: int = 48
-
-PROJECTILE_WIDTH: int = 48
-PROJECTILE_HEIGHT: int = 48
-
 GAP_BELOW_PLAYER: int = 24
+
+player = pygame.Rect(WINDOW_WIDTH / 2, PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 time = 0
 AFTER_WHAT_TIME_NEW_GUEST_VISITS = 3000
 BORDERS = 200
 
 last_time_new_guest_visits = 0
 
+projectiles = []
+PROJECTILE_WIDTH: int = 48
+PROJECTILE_HEIGHT: int = 48
 class Projectile:
   def __init__(self, x, y, dy):
-    self.x = x
-    self.y = y
+    self.rect = pygame.Rect(x, y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT)
     self.dy = dy
 
-projectiles = []
-
-class Guest:
-  def __init__(self, x, y, dy):
-    self.x = x
-    self.y = y
-    self.dy = dy
 
 guests = []
+GUEST_WIDTH: int = 48
+GUEST_HEIGHT: int = 48
+class Guest:
+  def __init__(self, x, y, dy):
+    self.rect = pygame.Rect(x, y, GUEST_WIDTH, GUEST_HEIGHT)
+    self.dy = dy
 
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-# space invader style character at bottom
-
-player_x = WINDOW_WIDTH / 2
-WHITE = (255, 255, 255)
-LIGHT_GREY = (100, 100, 100)
-GREEN = (0, 255, 0)
-
-BLUE = (0, 0, 128)
 pygame.display.set_caption('Masketeer')
 fontObj = pygame.font.Font('freesansbold.ttf', 32)
 
@@ -67,30 +60,30 @@ while True:  # main game loop
     # key handling
     keys = pygame.key.get_pressed()
     if keys[K_LEFT]:
-        player_x -= 1
+        player.x -= 1
     if keys[K_RIGHT]:
-        player_x += 1
+        player.x += 1
     if keys[K_SPACE]:
         # shoot mask
         if not projectiles:
-            projectiles.append(Projectile(player_x, WINDOW_HEIGHT - PLAYER_HEIGHT - GAP_BELOW_PLAYER, -1))
+            projectiles.append(Projectile(player.x, WINDOW_HEIGHT - player.height - GAP_BELOW_PLAYER, -1))
 
 
-    if player_x < BORDERS:
-        player_x = BORDERS
+    if player.x < BORDERS:
+        player.x = BORDERS
 
-    if player_x >= (max_x := (WINDOW_WIDTH - BORDERS - PLAYER_WIDTH)):
-        player_x = max_x
+    if player.x >= (max_x := (WINDOW_WIDTH - BORDERS - player.width)):
+        player.x = max_x
 
     pygame.draw.rect(screen, LIGHT_GREY,
                      pygame.Rect(0 + BORDERS, 0, WINDOW_WIDTH - BORDERS - BORDERS, WINDOW_HEIGHT))
 
     remove_projectiles = []
     for projectile in projectiles:
-        projectile.y += projectile.dy
+        projectile.rect.y += projectile.dy
         pygame.draw.rect(screen, "yellow",
-                       pygame.Rect(projectile.x, projectile.y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT))
-        if projectile.y < -PROJECTILE_HEIGHT:
+                       pygame.Rect(projectile.rect.x, projectile.rect.y, projectile.rect.width, projectile.rect.height))
+        if projectile.y < - projectile.rect.height:
             remove_projectiles.append(projectile)
     for projectile in remove_projectiles:
       projectiles.remove(projectile)
@@ -99,8 +92,8 @@ while True:  # main game loop
 
     remove_guests = []
     for guest in guests:
-      guest.y += guest.dy
-      if guest.y > WINDOW_HEIGHT + GUEST_HEIGHT:  # hit bottom of screen
+      guest.rect.y += guest.dy
+      if guest.rect.y > WINDOW_HEIGHT + guest.rect.height:  # hit bottom of screen
         remove_guests.append(guest)
     for guest in remove_guests:
       guests.remove(guest)
@@ -116,13 +109,13 @@ while True:  # main game loop
       pygame.draw.rect(
         screen,
         "red",
-        (guest.x, guest.y, GUEST_WIDTH, GUEST_HEIGHT)
+        (guest.rect.x, guest.rect.y, guest.rect.width, guest.rect.height)
       )
 
     pygame.draw.rect(
         screen,
         "blue",
-        (player_x, WINDOW_HEIGHT - PLAYER_HEIGHT - GAP_BELOW_PLAYER, PLAYER_WIDTH, PLAYER_HEIGHT)
+        (player.x, WINDOW_HEIGHT - player.height - GAP_BELOW_PLAYER, player.width, player.height)
     )
 
     pygame.display.update()
